98956f1fbcd4ae7bf4c58c2f4f351b78ea6d142d
gabrielforti <G@br13l_F0rt1>
2018-12-22 04:00:18 -0500

    updated test

--- gen-test.js
+++ gen-test.js
@@ -3,24 +3,30 @@ let nouns = require('./nouns')
 let verbs = require('./verbs')
 
 function coinFlip() {
-    return (Math.floor(Math.random() * 2) == 0);
+  return (Math.floor(Math.random() * 2) == 0);
 }
 
 function generateDraws() {
 
-    for (let i = 0; i < 6; i++ ){
-        let randAdv = Math.floor(Math.random() * adjectives.length)
-        let randNoun1 = Math.floor(Math.random() * nouns.length)
-        let randNoun2 = Math.floor(Math.random() * nouns.length)
-        let randVerb = Math.floor(Math.random() * verbs.length)
+  for (let i = 0; i < 6; i++) {
+    let randAdv = Math.floor(Math.random() * adjectives.en.length)
+    let randNoun1 = Math.floor(Math.random() * nouns.en.length)
+    let randNoun2 = Math.floor(Math.random() * nouns.en.length)
+    let randVerb = Math.floor(Math.random() * verbs.en.length)
 
-        if ( coinFlip() ) {
-            console.log(`Draw ${adjectives[randAdv]} ${nouns[randNoun1]}`)
-        } else {
-            console.log(`Draw a ${nouns[randNoun2]} that is going to ${verbs[randVerb]}`)
-        }
-
+    if (coinFlip()) {
+      console.log({
+        en: `Draw ${adjectives.en[randAdv]} ${nouns.en[randNoun1]}`,
+        es: `Dibujar ${adjectives.es[randAdv]} ${nouns.es[randNoun1]}`
+      })
+    } else {
+      console.log({
+        en: `Draw a ${nouns.en[randNoun2]} that will ${verbs.en[randVerb]}`,
+        es: `Dibujar un ${nouns.es[randNoun2]} que va a ${verbs.es[randVerb]}`,
+      })
     }
+
+  }
 }
 
 generateDraws()
